65ae63b5cd3adede1f600749ed8b5a49444d0e0e
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-05-22 17:54:09 -0400

    finished binary-search

---
+++ binary-search.js
@@ -0,0 +1,18 @@
+function BS(arr, target) {
+  let l = 0;
+  let h = arr.length - 1;
+  let mid;
+  while (l <= h) {
+    mid = Math.floor((l + h) / 2);
+    if (arr[mid] === target) return mid;
+
+    if (target > arr[mid]) {
+      l = mid + 1;
+    } else {
+      h = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(BS([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
