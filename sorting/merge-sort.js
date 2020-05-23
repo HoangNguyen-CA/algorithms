2254cdba2f4614ae00394db27027262b3f1e4e51
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-05-23 16:29:04 -0400

    recoded merge-sort to gain better understanding

--- sorting/merge-sort.js
+++ sorting/merge-sort.js
@@ -1,7 +1,8 @@
 function merge(arr, l, m, r) {
   let n1 = m - l + 1;
   let n2 = r - m;
-  //temp arrays;
+
+  //temp arrays
   let L = [];
   let R = [];
 
@@ -11,11 +12,12 @@ function merge(arr, l, m, r) {
   for (let j = 0; j < n2; j++) {
     R.push(arr[m + 1 + j]);
   }
+  console.log(L, R);
 
   let i = 0;
   let j = 0;
   let k = l;
-  //merge temp arrays;
+  //merge L and R
   while (i < n1 && j < n2) {
     if (L[i] <= R[j]) {
       arr[k++] = L[i++];
@@ -27,21 +29,24 @@ function merge(arr, l, m, r) {
   while (i < n1) {
     arr[k++] = L[i++];
   }
+
   while (j < n2) {
     arr[k++] = R[j++];
   }
 }
 
-function sort(arr, l, r) {
+function mergeSort(arr, l, r) {
   if (l < r) {
     let mid = Math.floor((l + r) / 2);
-    sort(arr, l, mid);
-    sort(arr, mid + 1, r);
+
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
     merge(arr, l, mid, r);
   }
 }
 
-let arr = [12, 11, 13, 5, 6, 7];
+let arr = [7, 5, 3, 9];
 
-sort(arr, 0, arr.length - 1);
+mergeSort(arr, 0, arr.length - 1);
+
 console.log(arr);
