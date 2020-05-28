e464ac78e8df1da3298aec964bea2fab1967956a
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-05-27 23:32:50 -0400

    letter case permutation

---
+++ backtracking/letter-case-permutation.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+var letterCasePermutation = function (S) {
+  let res = [];
+  backtrack(S, 0, [], res);
+  return res;
+};
+
+var backtrack = function (S, pos, currStr, res) {
+  if (currStr.length === S.length) {
+    res.push(currStr.join(''));
+    return;
+  } else {
+    let c = S[pos];
+
+    if (/^[A-Za-z]+$/.test(c)) {
+      currStr.push(c.toUpperCase());
+      backtrack(S, pos + 1, currStr, res);
+      currStr.pop();
+      currStr.push(c.toLowerCase());
+      backtrack(S, pos + 1, currStr, res);
+      currStr.pop();
+    } else {
+      currStr.push(c);
+      backtrack(S, pos + 1, currStr, res);
+      currStr.pop();
+    }
+  }
+};
+
+console.log(letterCasePermutation('a1b2'));
