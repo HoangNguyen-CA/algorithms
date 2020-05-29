15c26a11e0ff1282c318669c825c188ee8ae098a
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-05-28 23:36:09 -0400

    finished permutations-II

--- backtracking/perumutations-II.js
+++ backtracking/perumutations-II.js
@@ -2,8 +2,41 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function (nums) {};
+var permuteUnique = function (nums) {
+  let res = [];
+  backtrack(nums, 0, res);
+  return res;
+};
 
-function backtrack() {}
+function backtrack(nums, l, res) {
+  if (l >= nums.length - 1) {
+    res.push([...nums]);
+    return;
+  } else {
+    for (let i = l; i < nums.length; i++) {
+      let check = checkSwap(nums, l, i);
+      if (check) {
+        swap(nums, l, i);
+        backtrack(nums, l + 1, res);
+        swap(nums, l, i);
+      }
+    }
+  }
+}
 
-function swap() {}
+function checkSwap(nums, l, i) {
+  for (let j = l; j < i; j++) {
+    if (nums[i] === nums[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function swap(nums, i, j) {
+  let temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+}
+
+console.log(permuteUnique([1, 2, 1, 2]));
